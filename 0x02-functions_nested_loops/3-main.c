dc5b8e9b72d55e1e7f654955d6d282bb792a4378
Ahmedshaaban1114 <Ahmed@example.com>
2023-06-20 13:28:03 -0700

    G

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,18 +1,40 @@
-/**
-* main - check the code.
-*
-* Return: Always 0.
-*/
-int main(void)
-{
-int r;
+#include "main.h"
+		
 
-r = _islower('H');
-_putchar(r + '0');
-r = _islower('o');
-_putchar(r + '0');
-r = _islower(108);
-_putchar(r + '0');
-_putchar('\n');
-return (0);
+		
+/**
+		
+ * main - check the code.
+		
+ *
+		
+ * Return: Always 0.
+		
+ */
+		
+int main(void)
+		
+{
+		
+    int r;
+		
+
+		
+    r = _islower('H');
+		
+    _putchar(r + '0');
+		
+    r = _islower('o');
+		
+    _putchar(r + '0');
+		
+    r = _islower(108);
+		
+    _putchar(r + '0');
+		
+    _putchar('\n');
+		
+    return (0);
+		
 }
+
