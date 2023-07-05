a676a58a974820b592875ca52c92262c9ee0d83c
Ahmedshaaban1114 <Ahmed@example.com>
2023-07-05 06:41:26 -0700

    l

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,11 +3,12 @@
  * _print_rev_recursion - Prints a string in reverse
  * @s: The string to be printed.
  */
+
 void _print_rev_recursion(char *s)
 {
 	if (*s > '\0')
 	{
-		_puts_recursion(s + 1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
