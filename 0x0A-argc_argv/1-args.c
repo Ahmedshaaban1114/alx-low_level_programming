1e00cd735c474b3d61b07e02f7ee99dc976c4ca5
Ahmedshaaban1114 <Ahmed@example.com>
2023-07-11 04:22:51 -0700

    g

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - print the number of arguments passed to the program
- * @argc: number of arguments
- * @argv: array of arguments
+ *main - print the number of arguments passed to the program
+ *@argc: number of arguments
+ *@argv: array of arguments
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 
-int main (int argc , char const *argv[])
+int main (int argc, char *argv[] __attribute__((unused)))
 {
-	(void)argv;
-	printf("%d\n", argc);
-	return(0);
+	printf("%d\n", argc -1);
+	return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,7 +17,8 @@ int main(int argc, char *argv[])
 	{
 		for (c = argv[argc]; *c; c++)
 			if (*c < '0' || *c > '9')
-				return (printf("error\n"), 1);
+				printf("Error\n");
+				return (1);
 		sum += atoi(argv[argc]);
 	}
 	printf("%d\n", sum);
