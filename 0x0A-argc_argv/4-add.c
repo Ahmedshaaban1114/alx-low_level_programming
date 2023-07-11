29f8acb2c69b4ea5bc71a3cdb5f86f633734b4fb
Ahmedshaaban1114 <Ahmed@example.com>
2023-07-11 03:58:43 -0700

    k

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c > '9')
+				return (printf("error\n"), 1);
+		sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
