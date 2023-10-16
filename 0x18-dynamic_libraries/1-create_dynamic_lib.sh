e08afa483b32477d5b3b6ed6058b8104b0d0923a
Ahmedshaaban1114 <Ahmed@example.com>
2023-10-16 16:55:34 -0700

    F

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -fPIC-c *.c
-gcc  -shared *.o -o liball
+gcc -fPIC -c *.c
+gcc  -shared *.o -o liball.so
--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Ahmed1114/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+wget -P /tmp https://github.com/Ahmedshaaban114/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD= /tmp/libgiga.so
